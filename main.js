a769492687df66ed26a7ab201bf547b2b7f86ce5
brenobader <brenobader@gmail.com>
2021-05-26 16:03:28 -0300

    primeiro comentário

---
+++ main.js
@@ -0,0 +1,30 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela 
+// sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que
+// representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+let name = "Bader Zu"
+let idade = 26
+let ano = 2021-26
+let souHumano = true
+let number = 3
+let two = 29 
+
+console.log(`Meu Ano de Nascimento e, ${ano}`)
+console.log(`A soma das variáveis é, ${number+two}`)
+
+
+
+
+
+
+
